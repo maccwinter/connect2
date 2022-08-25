cf255d62c17bb57fff5013c036a7ad2b938036c4
maccwinter <matthew.winter1@louisiana.edu>
2022-08-24 20:57:33 -0500

    it worked!

--- hope.R
+++ hope.R
@@ -1,1 +1,6 @@
 #heres to hope
+
+#It worked!! What I had to do was, when pasting the URL from github 
+#to make a new project, I had to copy the "SSH" form, which 
+#basically doesn't have 'http' in it. It starts with "git.."
+#I also set up some public key, or ssh thing. 
